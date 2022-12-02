3af9db4bd510ee2161284870d7715f4e3579ab40
Abraham <cartes5ironman@gmail.com>
2022-12-02 17:34:15 +0100

    Estructurando vistas

---
+++ resources/js/components/Welcome.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="container mt-5">
+        <div class="col-12 text-center">
+            <h1>Cartes5Ironman</h1>
+            <a href="https://github.com/Cartes5" target="_blank">Visita para mas Gits</a>
+        </div>
+    </div>
+</template>
